099e097b832dc002d32c054a01dfc43164ec779a
Toktarla <111569015+Toktarla@users.noreply.github.com>
2023-04-15 21:14:24 +0300

    creating adding and size functions

--- src/assignment2/MyArrayList.java
+++ src/assignment2/MyArrayList.java
@@ -12,5 +12,40 @@ public class MyArrayList<T> implements MyList<T>{
         arr = new Object[initialCapacity];
     }
 
+    private void increastBuffer() {
+        int biggerSize = (int) (arr.length * 1.5);
+        Object[] newArr = new Object[biggerSize];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
+
+    @Override
+    public void add(Object item) {
+        if (size == arr.length) {
+            increastBuffer();
+        }
+        arr[size++] = item;
+    }
+
+    @Override
+    public void addAtIndex(Object item, int index) {
+        if (index >= size) throw new ArrayIndexOutOfBoundsException();
+        increastBuffer();
+        for (int i = size; i >= index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = item;
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+
 
 }

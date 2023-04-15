7c3c9921f2e6b63b78d2b18bce1d6a7f23c7fff9
Toktarla <111569015+Toktarla@users.noreply.github.com>
2023-04-15 21:37:22 +0300

    size and adding functions

--- src/assignment2/MyLinkedList.java
+++ src/assignment2/MyLinkedList.java
@@ -22,6 +22,59 @@ public class MyLinkedList<T> implements MyList<T> {
 
     public MyLinkedList(){}
 
+    // Returns the number of nodes in the linked list
+    @Override
+    public int size() {
+        return size;
+    }
+
+    // Adds a new node to the end of the linked list
+    @Override
+    public void add(T item) {
+
+        Node newNode = new Node(item,null,tail);
+        size++;
+        if(head == null){
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+
+    }
+
+    // Adds a new node at a specified index
+    @Override
+    public void addAtIndex(T item, int index) {
+        if(index == size){
+            add(item);
+            return;
+        }
+
+        Node newNode = new Node(item,null,null);
+        if(index == 0){
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+
+        } else{
+
+            Node current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            newNode.prev = current.prev;
+            newNode.next = current;
+            current.prev.next = newNode;
+            current.prev = newNode;
+
+
+        }
+        size++;
+    }
+
+
+
 
 
 }
